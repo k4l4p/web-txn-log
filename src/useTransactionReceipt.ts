48c7a6c6393a34ee9f663f8ad58298f1ac0c6cdb
k4l4p <kalapwong@gmail.com>
2025-01-09 11:53:04 +0800

    impl: kairos

--- src/useTransactionReceipt.ts
+++ src/useTransactionReceipt.ts
@@ -53,6 +53,31 @@ export const neox = defineChain({
 	},
 })
 
+export const kairos = /*#__PURE__*/ defineChain({
+	id: 1001,
+	name: "Kairos",
+	nativeCurrency: { name: "KAIA", symbol: "KAIA", decimals: 18 },
+	rpcUrls: {
+		default: {
+			http: ["https://public-en-kairos.node.kaia.io"],
+			webSocket: ["wss://public-en-kairos.node.kaia.io/ws"],
+		},
+	},
+	blockExplorers: {
+		default: {
+			name: "Kaiascope",
+			url: "https://kairos.kaiascope.com/",
+			apiUrl: "https://api-baobab.klaytnscope.com/api",
+		},
+	},
+	contracts: {
+		multicall3: {
+			address: "0x2b8B020c627816058052E9274D8cCE1acf215b2D",
+			blockCreated: 4299,
+		},
+	},
+})
+
 const neoxt4Client = createPublicClient({
 	transport: http(),
 	chain: neoXT4,
@@ -68,13 +93,19 @@ const arbClient = createPublicClient({
 	chain: arbitrum,
 })
 
-const chainMap = { neoxClient, neoxt4Client, arbClient }
+const kairosClient = createPublicClient({
+	transport: http(),
+	chain: kairos,
+})
+
+const chainMap = { neoxClient, neoxt4Client, arbClient, kairosClient }
 
 export type ChoosableChain = keyof typeof chainMap
 export const allChainName: Array<ChoosableChain> = [
 	"neoxClient",
 	"neoxt4Client",
 	"arbClient",
+	"kairosClient",
 ]
 
 interface UseTransactionReceiptReturn {
